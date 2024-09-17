d680c9b2467bdbc301eaa73430833ec5857176c5
Malika7188 <asmanodeny@gmail.com>
2024-09-17 00:16:23 -0400

    Start the REPL to allow users to input commands interactively

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Kisumu programming language!\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
+	fmt.Printf("Halo %s! This is the Kisumu programming language!\n", user.Username)
+	fmt.Printf("Type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
