3174952ca5f7a287e8e6f4aad06c031faea90477
Malika7188 <asmanodeny@gmail.com>
2024-09-23 01:15:44 -0400

    feat(parser): Implement parsing for operator expressions

--- parser/parsetoken.go
+++ parser/parsetoken.go
@@ -1,7 +1,9 @@
 package lexer
 
 import (
+	ast "ksm/Ast"
 	lexer "ksm/Lexer"
+	"strconv"
 )
 
 type Parser struct {
@@ -22,4 +24,117 @@ func (p *Parser) NextToken() {
 	p.currentToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 
+}
+
+// ParseProgram handles the entire program
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	p.NextToken() // Start fetching tokens
+
+	// Loop through all tokens until EOF
+	for p.currentToken.Type != lexer.EOF {
+		statement := p.parseStatement() // Parse different types of statements
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+		p.NextToken() // Fetch next token
+	}
+	return program
+}
+
+// ParseStatement decides which specific statement parser to call
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case lexer.LET:
+		return p.parseLetStatement()
+	case lexer.RETURN:
+		return p.parseReturnStatement()
+	default:
+		// Handle other cases or errors
+		return nil
+	}
+}
+
+// ParseLetStatement parses let statements like `let x = 5;`
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: p.currentToken}
+	if !p.expectPeek(lexer.IDENT) {
+		return nil
+	}
+	statement.Name = p.parseIdentifier()
+
+	if !p.expectPeek(lexer.ASSIGN) {
+		return nil
+	}
+
+	p.NextToken() // Move to the value of the let statement
+	statement.Value = p.parseExpression()
+	return statement
+}
+
+// ParseExpression parses expressions like integers, operations, etc.
+func (p *Parser) parseExpression() ast.Expression {
+	switch p.currentToken.Type {
+	case lexer.INT:
+		return p.parseIntegerLiteral()
+	case lexer.PLUS:
+		return p.parseOperatorExpression()
+	default:
+		// Handle other expression types
+		return nil
+	}
+}
+
+// Helper function to parse identifiers
+func (p *Parser) parseIdentifier() *ast.Identifiers {
+	return &ast.Identifiers{Token: p.currentToken, Value: p.currentToken.Literal}
+}
+
+// ParseOperatorExpression handles operations like 1 + 2
+func (p *Parser) parseOperatorExpression() ast.Expression {
+	expression := &ast.OperatorExpression{
+		Left:     p.parseIntegerLiteral(),
+		Operator: p.currentToken,
+	}
+	p.NextToken() // Move to next part of expression (right side)
+	expression.Right = p.parseExpression()
+	return expression
+}
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.currentToken} 
+
+	p.NextToken()
+
+	stmt.ReturnValue = p.parseExpression()
+
+	// We expect a semicolon after the return value
+	if p.peekTokenIs(lexer.SEMICOLON) {
+		p.NextToken()
+	}
+
+	return stmt
+}
+func (p *Parser) expectPeek(t lexer.TokenType) bool {
+	if p.peekToken.Type == t {
+		p.NextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) peekTokenIs(t lexer.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.currentToken}
+
+	value, err := strconv.ParseInt(p.currentToken.Literal, 0, 64)
+	if err != nil {
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
